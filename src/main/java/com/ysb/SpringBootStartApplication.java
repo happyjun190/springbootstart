4fb67561febe7a2a9e3b185281ad1790a4c4625b
happyjun190 <happyjun190@gmail.com>
2017-07-19 22:23:53 +0800

    完成基本配置，之前错误是因为没有在程序入口配置 注解 @SpringBootApplication

--- src/main/java/com/ysb/SpringBootStartApplication.java
+++ src/main/java/com/ysb/SpringBootStartApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by wushenjun on 2017/7/19.
  */
-@EnableCaching
-@RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class SpringBootStartApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(SpringBootStartApplication.class);
